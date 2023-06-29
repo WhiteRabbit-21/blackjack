617783cf342e6c98e0e1d13d01feefb36a8577bf
alyon <alyon@ASUS>
2023-06-29 13:31:31 +0200

    Have added count Function and deleted not functional part of code

--- ProjEU/FileName.cpp
+++ ProjEU/FileName.cpp
@@ -1,47 +1,75 @@
-#include <stdio.h>
 #include <iostream>
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
 #include <vector>
-#include <map>
-#include <unordered_map>
+#include <time.h>
 using namespace std;
 
-void initt(multimap<string, int>& card) {
-
-    card.insert(std::pair<string, int>("Deuce", 2));
-    card.insert(std::pair<string, int>("Three", 3));
-    card.insert(std::pair<string, int>("Four", 4));
-    card.insert(std::pair<string, int>("Five", 5));
-    card.insert(std::pair<string, int>("Six", 6));
-    card.insert(std::pair<string, int>("Seven", 7));
-    card.insert(std::pair<string, int>("Eight", 8));
+int countValues(string Card, int &sum, vector<string> face) {
+    
+    if (Card == "Deuce") {
+        sum += 2;
+    }
+    if (Card == "Three") {
+        sum += 3;
+    }
+    if (Card == "Four") {
+        sum += 4;
+    }
+    if (Card == "Five") {
+        sum += 5;
+    }
+    if (Card == "Six") {
+        sum += 6;
+    }
+    if (Card == "Seven") {
+        sum += 7;
+    }
+    if (Card == "Eight") {
+        sum += 8;
+    }
+    return sum;
 }
 
 int main(){
-    
+
+    srand(time(0));
+    // Intializing cards
     vector <string> face = 
     { "Ace", "Deuce", "Three", "Four",
       "Five", "Six", "Seven", "Eight",
       "Nine", "Ten", "Jack", "Queen", "King" };
 
-    //vector <string> suit = { "Hearts", "Diamonds", "Clubs", "Spades" };
+    vector <string> suit = { "Hearts", "Diamonds", "Clubs", "Spades" };
 
-    
-    multimap<string, int> card; 
-    initt(card);
-    
-    
-    multimap <string, int>::iterator itr;
+    // Generating card on the table for the player
 
-    for (itr = card.begin(); itr != card.end(); ++itr) {
-        std::cout << itr->first << ", " << itr->second;
-        std::cout << endl;
+    int i = 0, AmountCardOnTable = 2, sum = 0;
+    while (i < AmountCardOnTable) 
+    {
+        int tmp = rand() % face.size();
+        cout << face[tmp] << " " << suit[rand() % suit.size()] << endl;
+        i++;
+        countValues(face[tmp], sum, face);
+
+        if (i >= AmountCardOnTable) 
+        {
+            cout << "Do you need more?" << endl;
+            bool ans;
+            cin >> ans;
+            if (ans == 1) {
+                AmountCardOnTable++;
+            }
+            
+        }
     }
-    std::cout << endl;
- 
-    bool choice;
+
+    cout << endl;
+    cout << "Sum is: " << sum;
+    cout << endl;
+   // cout << face[rand() % face.size()] << " " << suit[rand() % suit.size()] << endl;
+   // cout << face[rand() % face.size()] << " " << suit[rand() % suit.size()] << endl;
 
    /* while (true) {
 
